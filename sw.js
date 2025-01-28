a451e185c42779697469b5e698a5553f15770259
James Risberg <james.risberg@gmail.com>
2025-01-27 19:01:02 -0800

    merge upstream

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "ollama-chat-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
